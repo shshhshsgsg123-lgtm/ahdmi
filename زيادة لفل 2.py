2c176a721de1da34d116b019450ac6a074a038ae
shshhshsgsg123-lgtm <shshhshsgsg123@gmail.com>
2025-11-16 04:40:47 +0100

    Add files via upload

---
+++ زيادة لفل 2.py
@@ -0,0 +1,30 @@
+import requests
+import time
+
+channel_id = "1258898445314424835"  # اكتب ID القناة
+auth_token = "NTY0OTk3MDA0NDIyMjgzMjk0.GJDZ2j.WwcpLE0DQApAknZTAdwgTrdRxT-_Zbf0s1TX6M"  # توكن حسابك العادي
+message_text = "Don't forget to drink water droplet"
+interval_seconds = 6  # 10 دقائق
+
+headers = {
+    "Authorization": auth_token
+}
+
+while True:
+    for i in range(2):
+        try:
+            payload = {"content": message_text}
+            response = requests.post(
+                f"https://discord.com/api/v9/channels/{channel_id}/messages",
+                headers=headers,
+                json=payload
+            )
+            if response.status_code == 200:
+                print(f"Message sent: {message_text}")
+            else:
+                print(f"Failed: {response.status_code} - {response.text}")
+        except Exception as e:
+            print(f"Error: {e}")
+        time.sleep(1)
+
+    time.sleep(interval_seconds)
